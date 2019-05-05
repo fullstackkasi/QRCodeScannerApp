97848e8a5245a1fef72611949270903f60542a93
Jinsol Kim <jskim@gdcomm.io>
2019-05-05 11:44:57 +0900

    Add QRCodeScanner Module

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -5,6 +5,7 @@ import { createStackNavigator, createBottomTabNavigator } from 'react-navigation
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
+import QRcodeScreen from '../screens/QRcodeScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 
 const HomeStack = createStackNavigator({
@@ -21,6 +22,20 @@ HomeStack.navigationOptions = {
           ? `ios-information-circle${focused ? '' : '-outline'}`
           : 'md-information-circle'
       }
+    />
+  ),
+};
+
+const QRcodeStack = createStackNavigator({
+  QRcode: QRcodeScreen,
+});
+
+QRcodeStack.navigationOptions = {
+  tabBarLabel: 'QRcode',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-qr-scanner' : 'md-qr-scanner'}
     />
   ),
 };
@@ -55,6 +70,7 @@ SettingsStack.navigationOptions = {
 
 export default createBottomTabNavigator({
   HomeStack,
+  QRcodeStack,
   LinksStack,
   SettingsStack,
 });
